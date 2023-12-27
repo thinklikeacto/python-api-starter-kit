f4188ac3a924ff27c0d07521b9f169e0309a4a77
Naresh Thakur <thinklikeacto@gmail.com>
2023-12-27 23:56:59 +0530

    added connection to mongodb
    - if connection fail, app will not start
    - need to connection with kafka
    - need to add crud apis

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pymongo import MongoClient
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 app = FastAPI()
 
+# MongoDB setup
+MONGO_DETAILS = os.getenv("MONGO_DETAILS")  # MongoDB connection string
+print("MONGO_DETAILS is: ", MONGO_DETAILS)
+mongo_client = MongoClient(MONGO_DETAILS)
+
+@app.on_event("startup")
+async def startup_event():
+    global kafka_producer
+    # Connect to MongoDB
+    try:
+        mongo_client.server_info()  # Force a call to check the connection
+    except Exception as e:
+        print(f"MongoDB connection failed: {e}")
+        raise HTTPException(status_code=500, detail=f"MongoDB connection failed: {e}")
+    
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
